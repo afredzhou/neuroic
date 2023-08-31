b0a5adc5f3407138db3a07ceeeb1b971d6d10e9b
afredzhou <afred.zhou.cn@gmail.com>
2023-08-31 19:21:51 +0800

    add dependence

--- Neuronic.ino
+++ Neuronic.ino
@@ -1,24 +1,20 @@
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEAdvertising.h>
-
-#define UUID 1836475137
+#include "setup.h"
+#include "loop.h"
+#define UUID  1836475137
 
 void setup() {
   Serial.begin(115200);
-
   // 初始化WiFi和其他设置
   wifisetup();
-
   // 初始化BLE设备
   BLEDevice::init("E32");
-
   // 创建BLE服务器
   BLEServer *pServer = BLEDevice::createServer();
-  
   // 创建BLE服务
   BLEService *pService = pServer->createService(BLEUUID((uint16_t)UUID));
-
   // 创建BLE特征
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
                                        BLEUUID((uint16_t)(UUID + 1)),
