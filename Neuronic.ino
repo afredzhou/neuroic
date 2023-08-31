73d53fb79a6f38b065d2ade6dc10e1af4bfe6aba
afredzhou <afred.zhou.cn@gmail.com>
2023-08-31 19:32:58 +0800

    add dependence

--- Neuronic.ino
+++ Neuronic.ino
@@ -1,37 +1,64 @@
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEAdvertising.h>
 #include "setup.h"
 #include "loop.h"
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 #define UUID  1836475137
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
 
 void setup() {
   Serial.begin(115200);
   // 初始化WiFi和其他设置
   wifisetup();
   // 初始化BLE设备
-  BLEDevice::init("E32");
-  // 创建BLE服务器
-  BLEServer *pServer = BLEDevice::createServer();
-  // 创建BLE服务
-  BLEService *pService = pServer->createService(BLEUUID((uint16_t)UUID));
-  // 创建BLE特征
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                       BLEUUID((uint16_t)(UUID + 1)),
-                                       BLECharacteristic::PROPERTY_READ |
-                                       BLECharacteristic::PROPERTY_WRITE
-                                       );
-  
-  // 启动BLE服务
+   BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+    BLEService *pService = pServer->createService(BLEUUID((uint16_t)UUID));
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                     BLEUUID((uint16_t)(UUID + 1)),
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
   pService->start();
 
-  // 启动BLE广播
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->addServiceUUID(pService->getUUID());
-  pAdvertising->setScanResponse(true);
-  pAdvertising->start();
-
-  Serial.println("Started advertising");
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(BLEUUID((uint16_t)UUID));
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
